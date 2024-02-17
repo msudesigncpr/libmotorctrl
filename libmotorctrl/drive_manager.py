f84a643a51d7cc29b01967dab5861938f73c8f7c
William Culhane <will@culhane.top>
2024-02-17 11:59:43 -0700

    Add docstrings for all public methods

--- libmotorctrl/drive_manager.py
+++ libmotorctrl/drive_manager.py
@@ -1,3 +1,5 @@
+"""Wrapper for all system-level drive commands via the `DriveManager` class."""
+
 import asyncio
 import logging
 from enum import Enum
@@ -8,22 +10,47 @@ from .drive import Drive
 # TODO Clamp values
 # TODO Refactor parse/write
 
-CRUISE_DEPTH = (
-    20_000  # Micrometers # A safe height above the board for general movement
-)
+CRUISE_DEPTH = 20_000
+"""A safe depth for the z-axis to descend to where it cannot collide with any obstacles.
 
-CALIBRATION_OFFSET = (0, 90_000)  # Micrometers # TODO Get this from calibration
+The z-axis will be moved to this height when executing movement commands before
+adjusting the x and y-axes. Specified in micrometers."""
+
+CALIBRATION_OFFSET = (0, 90_000)  # TODO Get this from calibration
+"""The location of the calibration point in (x,y) format.
+
+This is applied as an offset to all movement commands, before the coordinates
+are sent to their respective drive controllers."""
 
 
-# A drive target to be used for methods which require specifying a
-# target drive
 class DriveTarget(Enum):
+    """A drive target used for methods which require specifying a target drive."""
+
     DriveX = 0
     DriveY = 1
     DriveZ = 2
 
 
 class DriveManager:
+    """Wrapper for system-level commands to the drives.
+
+    Generally, to use the system you will need to
+    1. Create a `DriveManager` object,
+    2. Initialize the drives and
+    3. Home each of the drives in series
+
+    before the system is ready to receive movement commands.
+
+    All coordinates this class interacts with MUST be provided in micrometers.
+    The drive controllers interpret the coordinates they receive as um offsets
+    from the origin point set with the Festo Configuration Tool. Coordinates
+    received by any methods in this class should be micrometer offsets from the
+    calibration origin point, which is found by recognizing the location of the
+    pinhole light with the camera.
+
+    Once the full sampling run is complete, the drives can be disabled by
+    calling the `terminate()` method."""
+
     def __init__(self):
         """Initialize the drives.
 
@@ -32,9 +59,9 @@ class DriveManager:
         to read and write the registers to/from the drive."""
 
         logging.info("Spawning drive controllers...")
-        self.drive_x = Drive("X", "192.168.2.21")
-        self.drive_y = Drive("Y", "192.168.2.22")
-        self.drive_z = Drive("Z", "192.168.2.23")
+        self._drive_x = Drive("X", "192.168.2.21")
+        self._drive_y = Drive("Y", "192.168.2.22")
+        self._drive_z = Drive("Z", "192.168.2.23")
 
     async def init_drives(self):
         """Initializes the drive registers to prepare them for positioning.
@@ -44,9 +71,9 @@ class DriveManager:
         drive for direct positioning, and enable movement."""
 
         async with asyncio.TaskGroup() as init_tg:
-            init_tg.create_task(self.drive_x.initialize_reg())
-            init_tg.create_task(self.drive_y.initialize_reg())
-            init_tg.create_task(self.drive_z.initialize_reg())
+            init_tg.create_task(self._drive_x.initialize_reg())
+            init_tg.create_task(self._drive_y.initialize_reg())
+            init_tg.create_task(self._drive_z.initialize_reg())
         logging.info("All drives initialized")
 
     async def home(self, drive):
@@ -58,11 +85,11 @@ class DriveManager:
 
         match drive:
             case DriveTarget.DriveX:
-                await self.drive_x.home()
+                await self._drive_x.home()
             case DriveTarget.DriveY:
-                await self.drive_y.home()
+                await self._drive_y.home()
             case DriveTarget.DriveZ:
-                await self.drive_z.home()
+                await self._drive_z.home()
 
     async def move(self, target_x, target_y, target_z):
         """Move to the designated coordinates.
@@ -72,41 +99,47 @@ class DriveManager:
         will raise the z-axis to the specified `CRUISE_DEPTH`, move
         the x and y axes, and then lower the z-axis to `target_z`."""
 
-        await self.drive_z.move(CRUISE_DEPTH)
+        await self._drive_z.move(CRUISE_DEPTH)
         logging.info("Drive Z raised to cruise depth")
 
         # Run the X and Y motions concurrently
         async with asyncio.TaskGroup() as move_tg:
-            move_tg.create_task(self.drive_x.move(target_x + CALIBRATION_OFFSET[0]))
-            move_tg.create_task(self.drive_y.move(target_y + CALIBRATION_OFFSET[1]))
+            move_tg.create_task(self._drive_x.move(target_x + CALIBRATION_OFFSET[0]))
+            move_tg.create_task(self._drive_y.move(target_y + CALIBRATION_OFFSET[1]))
         logging.info("XY motion complete")
 
-        await self.drive_z.move(target_z)
+        await self._drive_z.move(target_z)
         logging.info("Z motion complete")
 
     async def stop(self):
-        """Immediately stop all movement."""
+        """Immediately stop all movement.
+
+        This sets the halt bit on all drives, and they will need to be reset
+        before they can be moved again."""
 
         async with asyncio.TaskGroup() as stop_tg:
-            stop_tg.create_task(self.drive_x.stop())
-            stop_tg.create_task(self.drive_y.stop())
-            stop_tg.create_task(self.drive_z.stop())
+            stop_tg.create_task(self._drive_x.stop())
+            stop_tg.create_task(self._drive_y.stop())
+            stop_tg.create_task(self._drive_z.stop())
 
     async def stop_drive(self, drive):
-        """Immediately stop the specified drive."""
+        """Immediately stop the specified drive.
+
+        This sets the halt bit on all drives, and they will need to be reset
+        before they can be moved again."""
 
         match drive:
             case DriveTarget.DriveX:
-                await self.drive_x.stop()
+                await self._drive_x.stop()
             case DriveTarget.DriveY:
-                await self.drive_y.stop()
+                await self._drive_y.stop()
             case DriveTarget.DriveZ:
-                await self.drive_z.stop()
+                await self._drive_z.stop()
 
     async def terminate(self):
         """Disable a drive and terminate the Modbus connection."""
 
         async with asyncio.TaskGroup() as term_tg:
-            term_tg.create_task(self.drive_x.terminate())
-            term_tg.create_task(self.drive_y.terminate())
-            term_tg.create_task(self.drive_z.terminate())
+            term_tg.create_task(self._drive_x.terminate())
+            term_tg.create_task(self._drive_y.terminate())
+            term_tg.create_task(self._drive_z.terminate())
