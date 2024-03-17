e9e4f09cb8ca141377a0d6f4681bb3c7f22314ac
William Culhane <will@culhane.top>
2024-03-17 14:54:56 -0600

    Migrate from setuptools to pyproject.toml

--- libmotorctrl/__init__.py
+++ libmotorctrl/__init__.py
@@ -1,2 +1,5 @@
+import importlib.metadata
 from .drive_manager import DriveManager, DriveTarget
 from .drive import DriveState, DriveError, DriveActionError
+
+__version__ = importlib.metadata.version(__package__ or __name__)
