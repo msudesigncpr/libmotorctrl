d1d6ccad1823701aa4f2fa9d42e557d7a0d40bb7
William Culhane <will@culhane.top>
2024-03-19 13:13:59 -0600

    Remove version metadata

--- libmotorctrl/__init__.py
+++ libmotorctrl/__init__.py
@@ -1,5 +1,2 @@
-import importlib.metadata
 from .drive_manager import DriveManager, DriveTarget
 from .drive import DriveState, DriveError, DriveActionError
-
-__version__ = importlib.metadata.version(__package__ or __name__)
